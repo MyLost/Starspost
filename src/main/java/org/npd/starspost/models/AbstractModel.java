75fb7e0fe189c3d826299ff14cdbf216cfd53c8a
midas <ddimitrov10@gmail.com>
2025-03-18 09:46:22 +0200

    WIP

---
+++ src/main/java/org/npd/starspost/models/AbstractModel.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.npd.starspost.models;
+
+import jakarta.persistence.MappedSuperclass;
+
+/**
+ *
+ * @author midas
+ */
+@MappedSuperclass
+public abstract class AbstractModel implements Model {
+
+}
