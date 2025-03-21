cc01dc3a44e6ea77ee904c98329802d7d9e05f80
midas <ddimitrov10@gmail.com>
2025-03-21 11:13:34 +0200

    WIP

---
+++ src/main/java/org/npd/starspost/entities/BaseEntity.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.npd.starspost.entities;
+
+/**
+ *
+ * @author midas
+ * @param <Model>
+ */
+public abstract class BaseEntity<Model> implements IBaseEntity {
+}
